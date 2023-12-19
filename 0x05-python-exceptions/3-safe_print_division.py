aec294b3c67d6110e921590ed85676975a986756
WESTLEY <eugenewestley95@gmail.com>
2023-12-19 02:44:15 -0800

    Benamit

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/bin/usr/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+        print("Error: Division by zero")
+    except Exception as e:
+        result = None
+        print(f"Error: {e}")
+    finally:
+        print("Inside result: {}".format(result))
+        return result
