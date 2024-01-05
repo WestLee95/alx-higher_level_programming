d48c825ba3796e252ff593bfaff963726241ec47
WESTLEY <eugenewestley95@gmail.com>
2024-01-05 02:47:02 -0800

    RECT

---
+++ 0-run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Path to your Python script
+PYTHON_SCRIPT_PATH="/main.py"
+
+#Python command to run your script
+python $PYTHON_SCRIPT_PATH
---
+++ GG.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def my_func(counter=89):
+    return counter + 1
+ 
+print(my_func())
+
+my_func
